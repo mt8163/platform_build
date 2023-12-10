c8298df8959d9919eeeaab3e08c04484def1d947
Kai Jones <19639817+488315@users.noreply.github.com>
2023-12-09 17:26:35 -0800

    Exclude Camera2 for AOSP builds
    Change-Id: I64daac3c0ba5511dea6fb9abcb204d679fe01bef
    Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -24,7 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 PRODUCT_PACKAGES += \
     Browser2 \
     Calendar \
-    Camera2 \
     Contacts \
     DeskClock \
     Gallery2 \
@@ -34,6 +33,12 @@ PRODUCT_PACKAGES += \
     QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
+
+ifeq ($(AOSP_BUILD),)
+PRODUCT_PACKAGES += \
+    Camera2 \
+    LatinIME
+endif
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug
